73dea611008826108125676f10f197c276141ee0
Dmitry Grigorev <d-d12@mail.ru>
2023-08-30 21:47:18 +0400

    дораб js

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@ app.UseDefaultFiles(); // поддержка страниц html по умолчанию
 app.UseStaticFiles(); // добавляем поддержку статических файлов
 
 
+app.Map("/getFile.png", () =>
+{
+    string path = "Pic/P1.png";
+    string contentType = "image/png";
+    string downloadName = "File.png";
+    return Results.File(path, contentType, downloadName);
+});
 
-app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
+
 
 app.Run();
+
+
 
 
