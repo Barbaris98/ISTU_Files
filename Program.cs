b5bd02a20b7ebc84ba92d6e7b251f1de14fec163
Dmitry Grigorev <d-d12@mail.ru>
2023-08-30 20:00:38 +0400

    index, картинки, работает

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.UseDefaultFiles(); // поддержка страниц html по умолчанию
 app.UseStaticFiles(); // добавляем поддержку статических файлов
 
 
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
 
 app.Run();
 
